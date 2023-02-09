3010ab4e2b599ea4564f82c3f5d9ff5837ebd35e
manideepchalla08 <124772194+manideepchalla08@users.noreply.github.com>
2023-02-09 14:58:27 +0530

    Add files via upload

---
+++ day2/1.py
@@ -0,0 +1,11 @@
+def fibonacci (n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci (n-1) + fibonacci (n-2)
+def waycount(n):
+    return fibonacci(n+1)
+n=int(input("entre the value"))
+print ("The number of ways to reach the top of a staircase with", n, "steps is", fibonacci (n))
